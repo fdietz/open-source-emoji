f67afabdcd918086402e888c0b563fd708445592
timwhitlock <timwhitlock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-04 14:41:44 +0000

    released 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/open-source-emoji/tags/1.0.1@722591 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ emoji.php
@@ -0,0 +1,128 @@
+<?php
+/*
+Plugin Name: Open Source Emoji
+Plugin URI: http://wordpress.org/extend/plugins/open-source-emoji/
+Description: Open Source Emoji - add Android and Phantom Emoji to your blog for all devices.
+Author: Tim Whitlock
+Version: 1.0.1
+Author URI: http://timwhitlock.info/
+*/
+
+
+
+/**
+ * Get plugin local base directory in case __DIR__ isn't available (php<5.3)
+ */
+function os_emoji_basedir(){
+    static $dir;
+    isset($dir) or $dir = dirname(__FILE__);
+    return $dir;    
+}    
+
+
+
+/** 
+ * Include a component from the lib directory.
+ * @param string $component e.g. "admin"
+ * @return mixed value from last included file
+ */
+function os_emoji_include(){
+    $dir = os_emoji_basedir();
+    $ret = '';
+    foreach( func_get_args() as $component ){
+        $ret = include_once $dir.'/lib/emoji-'.$component.'.php';
+    }
+    return $ret;
+} 
+
+
+
+/**
+ * Get config options from DB
+ * @param array any new options to update
+ */
+function _os_emoji_config( array $update = array() ){
+    static $conf;
+    if( ! isset($conf) ){
+        $conf = array (
+            'theme' => 'android',
+        );
+        foreach( $conf as $key => $val ){
+            $conf[$key] = get_option('os_emoji_'.$key) or
+            $conf[$key] = $val;
+        }
+    }
+    foreach( $update as $key => $val ){
+        if( isset($conf[$key]) ){
+            update_option( 'os_emoji_'.$key, $val );
+            $conf[$key] = $val;
+        }
+    }
+    return $conf;
+}
+
+
+
+/**
+ * Convert emoji name to utf-8 character sequence
+ */
+function os_emoji_string( $name ){
+    static $map;
+    if( ! isset($map) ){
+        $map = os_emoji_include('named');
+    }
+    $key = strtr( strtoupper($name),'_',' ');
+    if( ! isset($map[$key]) ){
+        // get closest match
+        $grep = '/^'.preg_replace('/[^A-Z]+/','[^A-Z]+', preg_quote($key,'/') ).'/';
+        foreach( preg_grep( $grep, array_keys($map) ) as $key ){
+            return $map[$key];
+        }
+        return '';
+    }
+    return $map[$key];
+}
+
+
+
+/**
+ * Convert [emoji ..] short code into HTML
+ */
+function os_emoji_shortcode( $atts ){
+    return os_emoji_string( implode( ' ', $atts ) );
+}
+
+add_shortcode( 'emoji', 'os_emoji_shortcode' );
+
+
+
+
+/**
+ * Add JavaScript API to all pages
+ */
+function os_emoji_enqueue_scripts(){
+    static $hook;
+    if( isset($hook) ){
+        wp_dequeue_script($hook); // <- permits theme change
+    }
+    else {
+        $hook = 'os-emoji';
+    }
+    extract( _os_emoji_config() );
+    $js = plugins_url( 'pub/js/emoji.js', __FILE__ )
+        .'?theme='.apply_filters( 'emoji_theme', $theme );
+    wp_enqueue_script( $hook, $js, array(), false, true );    
+}
+
+
+
+
+/**
+ * Inialize admin screen
+ */
+if( is_admin() ){
+    os_emoji_include('admin');
+}
+else {
+    add_action( 'wp_enqueue_scripts', 'os_emoji_enqueue_scripts' );
+}
---
+++ readme.txt
@@ -0,0 +1,75 @@
+=== Plugin Name ===
+Contributors: timwhitlock
+Donate link: http://timwhitlock.info/donate-to-a-project/
+Tags: emoji, emoticons, icons, android, phantom, shortcode, unicode
+Requires at least: 3.5.1
+Tested up to: 3.5.1
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Open source Emoji plugin.
+
+== Description ==
+
+This plugin uses open source Emoji so it's fully legal to use without breaching copyright of Emoji artworks. 
+
+Choose between the Android style and the full colour [Phantom](http://www.kickstarter.com/projects/374397522/phantom-open-emoji) set.
+
+All Emoji on your pages are converted to visible icons, not just the ones you add in your posts.
+
+Built by [timwhitlock](https://twitter.com/timwhitlock)
+
+
+== Installation ==
+
+1. Unzip all files to the `/wp-content/plugins/` directory
+2. Log into Wordpress admin and activate the 'Open Source Emoji' plugin through the 'Plugins' menu
+3. Go to *Settings > OS Emoji* to change the Emoji icon styles.
+
+= Adding Emoji to your posts =
+
+To enter Emoji into your posts, use the [emoji ..] shortcode with the name of the Emoji following it. e.g. `[emoji cat face]`.
+
+See the [full list of supported Emoji codes](http://apps.timwhitlock.info/emoji/tables/unicode). Enter the description that appears in the right hand column.
+
+== Frequently Asked Questions ==
+
+= Are the Emoji icons copyright? =
+
+No. This plugin uses only open source licensed artworks. See the Credits tab.
+
+= Why are some icons missing? =
+
+The Android Emoji set is [missing some characters](http://timwhitlock.info/blog/2013/04/androids-missing-emoji/) that you'll find on other systems, but it does follow the published Unicode standard.
+
+The Phantom Emoji set is very new and the team are still adding new icons. Keep this plugin updated to get the new icons as they're created.
+
+=  Why do the icons appear the wrong size compared to my text? =
+
+The Phantom Emoji set uses fixed size images at 22px.  I'm working on making them scalable.
+
+The Android Emoji is scalable because it's a font. If the fixed size Emoji don't work for you, consider using that.
+
+
+= Are these FAQs complete? =
+
+No. I'm working on them. Feel free to ask a question in the Support tab, or on [Twitter](https://twitter.com/timwhitlock)
+
+
+== Changelog ==
+
+= 1.0.0 =
+* First version released
+
+= 1.0.1 =
+* Added preview table
+
+== Credits ==
+
+[Phantom Emoji](http://www.kickstarter.com/projects/374397522/phantom-open-emoji)
+is an open source set of pictographs, [see license](https://github.com/Genshin/PhantomOpenEmoji/blob/master/LICENSE.md)
+                    
+Android Emoji font Copyright © 2008 The Android Open Source Project.
+Licensed under the [Apache License](http://www.apache.org/licenses/LICENSE-2.0). 
+See [notice](https://s3-eu-west-1.amazonaws.com/tw-font/android/NOTICE)
